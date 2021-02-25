b253cbb74c562bf24b8301b13716eaa999fb474d
helen-huangg <huangg.helen@gmail.com>
2021-02-25 19:50:29 +0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,8 +4,44 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1 align="center">Form Pembeliah Buah</h1>
+      <table cellpadding="10" cellspacing="10">
+        <tr>
+          <td>
+            <b>Nama Pelanggan</b>
+          </td>
+          <td>
+            <input type="text" />
+          </td>
+        </tr>
+        <tr>
+          <td class="vertical">
+            <b>Daftar Item</b>
+          </td>
+          <td>
+            <input type="checkbox" />
+            Semangka
+            <br />
+            <input type="checkbox" />
+            Jeruk
+            <br />
+            <input type="checkbox" />
+            Nanas
+            <br />
+            <input type="checkbox" />
+            Salak
+            <br />
+            <input type="checkbox" />
+            Anggur
+            <br />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="submit" value="Kirim" class="tombol" />
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
