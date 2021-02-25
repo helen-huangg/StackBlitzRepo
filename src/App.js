5ca4ef452b160cc26f2f4cb999d476506e9aca07
helen-huangg <huangg.helen@gmail.com>
2021-02-25 20:06:12 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,42 +5,44 @@ export default function App() {
   return (
     <div>
       <h1 align="center">Form Pembeliah Buah</h1>
-      <table cellpadding="10" cellspacing="10">
-        <tr>
-          <td>
-            <b>Nama Pelanggan</b>
-          </td>
-          <td>
-            <input type="text" />
-          </td>
-        </tr>
-        <tr>
-          <td class="vertical">
-            <b>Daftar Item</b>
-          </td>
-          <td>
-            <input type="checkbox" />
-            Semangka
-            <br />
-            <input type="checkbox" />
-            Jeruk
-            <br />
-            <input type="checkbox" />
-            Nanas
-            <br />
-            <input type="checkbox" />
-            Salak
-            <br />
-            <input type="checkbox" />
-            Anggur
-            <br />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <input type="submit" value="Kirim" class="tombol" />
-          </td>
-        </tr>
+      <table cellPadding="10" cellSpacing="10">
+        <tbody>
+          <tr>
+            <td>
+              <b>Nama Pelanggan</b>
+            </td>
+            <td>
+              <input type="text" />
+            </td>
+          </tr>
+          <tr>
+            <td className="vertical">
+              <b>Daftar Item</b>
+            </td>
+            <td>
+              <input type="checkbox" />
+              Semangka
+              <br />
+              <input type="checkbox" />
+              Jeruk
+              <br />
+              <input type="checkbox" />
+              Nanas
+              <br />
+              <input type="checkbox" />
+              Salak
+              <br />
+              <input type="checkbox" />
+              Anggur
+              <br />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input type="submit" value="Kirim" className="tombol" />
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
